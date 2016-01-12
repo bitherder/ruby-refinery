08687d1ff255c349e0a13c179ad9f8029c295ccd
Larry Baltz <larry@baltz.org>
2016-01-13 00:28:32 +0100

    remove cut and paste code

--- lib/ruby_refinery/standard/uri/generic/instance/query_equals.rb
+++ lib/ruby_refinery/standard/uri/generic/instance/query_equals.rb
@@ -1,21 +1,6 @@
 module RubyRefinery
   refine URI::Generic do
     # TODO: use better aliasing
-    alias :query_without_ruby_refinery :query
-    def query
-      result = query_without_ruby_refinery
-      class << result
-        def to_a
-          URI.decode_www_form(self)
-        end
-
-        def to_h
-          to_a.to_h
-        end
-      end
-      result
-    end
-
     alias :query_equals_without_ruby_refinery :query=
     def query=(value)
       if value.kind_of? Enumerable
