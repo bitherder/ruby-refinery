04dc5d54c86ab184f645aee4e7f56a507a624292
Larry Baltz <larry.baltz@xing.com>
2016-01-29 18:12:10 +0100

    fill out require specs

---
+++ spec/lib/core_ext/uri/class/class_spec.rb
@@ -0,0 +1,8 @@
+require_relative '../../../../spec_helper.rb'
+
+describe 'refinery/core_ext/uri/class' do
+  it 'can be required' do
+    require 'refinery/core_ext/uri/class'
+  end
+end
+
---
+++ spec/lib/core_ext/uri/generic/instance/instance_spec.rb
@@ -0,0 +1,8 @@
+require_relative '../../../../../spec_helper.rb'
+
+describe 'refinery/core_ext/uri/generic/instance' do
+  it 'can be required' do
+    require 'refinery/core_ext/uri/generic/instance'
+  end
+end
+
