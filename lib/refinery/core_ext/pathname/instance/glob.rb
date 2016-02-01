72919411b060b3533b552eb67d35f75b9acf5c25
Larry Baltz <larry.baltz@xing.com>
2016-02-01 18:45:08 +0100

    checkpoint

--- lib/refinery/core_ext/pathname/instance/glob.rb
+++ lib/refinery/core_ext/pathname/instance/glob.rb
@@ -1,7 +1,22 @@
 module Refinery
-  refine Pathname do
-    def glob(pattern)
-      Dir.glob(self+pattern).map{|result| self.class.new(result)}
+  module PathnameRefinement
+    module Instance
+      module Glob
+        refine Pathname do
+          def glob(pattern)
+            Dir.glob(self+pattern).map{|result| self.class.new(result)}
+          end
+        end
+      end
     end
   end
 end
+
+container = Refinery::PathnameRefinement::Instance::Glob
+container_string = container.to_s
+while /::/ =~ container_string
+  container_string.sub!(/::[^:]*$/, '')
+  container = eval(container_string)
+  container.include Refinery::PathnameRefinement::Instance::Glob
+end
+
